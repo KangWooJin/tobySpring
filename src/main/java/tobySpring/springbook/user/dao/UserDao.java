4d45b1a06b8560c581c3102f93ebfb0d323b7e4d
woojin.kang <woojin.kang@linecorp.com>
2018-02-22 03:05:56 +0900

    modify connector

--- src/main/java/tobySpring/springbook/user/dao/UserDao.java
+++ src/main/java/tobySpring/springbook/user/dao/UserDao.java
@@ -12,7 +12,7 @@ public class UserDao {
 	
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql//locahost/toby","root","1234");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby","root","1234");
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) value(?,?,?)");
 		ps.setString(1, user.getId());
@@ -27,8 +27,8 @@ public class UserDao {
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql//locahost/toby","root","1234");
-		
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby","root","1234");
+		//"jdbc:mysql://hostname:port/dbname","username", "password"
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 		
